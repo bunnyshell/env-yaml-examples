b8b948522562d0423cfbcd928d5401fe74e0b1a2
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2022-11-14 23:09:25 +0200

    PHP App - added Mongo check

--- apps/php/index.php
+++ apps/php/index.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+
 $connectionStatus = [
     'mysql'    => null,
     'mongo'    => null,
@@ -7,6 +10,9 @@ $connectionStatus = [
     'rabbitmq' => null,
 ];
 
+/**
+ * Check MySQL connectivity
+ */
 try {
     $dbh = new PDO(
         sprintf('mysql:host=%s:3306;dbname=%s', getenv('MYSQL_HOST'), getenv('MYSQL_DATABASE')),
@@ -18,6 +24,27 @@ try {
     $connectionStatus['mysql'] = true;
 } catch (PDOException $e) {
     $connectionStatus['mysql'] = false;
+}
+
+/**
+ * Check MongoDB connectivity
+ */
+$mongoClient = new MongoDB\Client(
+    sprintf(
+        'mongodb://%s:%s@%s:27017',
+        getenv('MONGO_USER'),
+        getenv('MONGO_PASS'),
+        getenv('MONGO_HOST')
+    )
+);
+
+try {
+    $dbs = $mongoClient->listDatabases();
+
+    $connectionStatus['mongo'] = true;
+}
+catch(Exception $e){
+    $connectionStatus['mongo'] = false;
 }
 
 header('Content-Type: application/json');
